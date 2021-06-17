fcb15e20e8e6e63305355a6ff993ee3d6b08e8dc
cphork <christina.phork@gmail.com>
2021-06-17 01:55:24 -0500

    added props to see if deployment works

--- pages/favorite.js
+++ pages/favorite.js
@@ -7,11 +7,14 @@ const Favorite = ({ faveWorkout, deleteFaveWorkout }) => {
         <div>
             <div>
                 <h2 className='text-center text-2xl mt-16 font-perm'>Favorite Arms Workouts List</h2>
-                {faveWorkout.map((arm, index) => (
+                {this.props.faveWorkout.map((arm, index) => (
                     <article>
                         <h5 className='mt-16 text-center text-2xl font-perm text-red-200'>{arm.title}</h5>
+
                         <h5 className='text-center text-lg font-mono font-mono text-red-200'>{arm.description}</h5>
+
                         <h5 className='mt-8 ml-8 mr-8 text-center text-base mb-16 font-hyeon font-perm text-red-200'>{arm.list}</h5>
+
                         <button
                             className='heart-btn ml-44 bg-red-400 rounded-full h-12 w-14 mb-24'
                             onClick={() =>
