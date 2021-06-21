2095e1c1f041a3e2c745200c4b931fe019de8b50
cphork <christina.phork@gmail.com>
2021-06-21 16:38:22 -0500

    testing link tag for deployment

--- pages/index.js
+++ pages/index.js
@@ -98,11 +98,11 @@ export default function Home() {
 
 
       {/* BLOGS LINKED TO EXTERNAL SOURCE */}
-      {/* <p className='font-perm text-center text-2xl text-green-200'>Fitness Blogs</p>
-      <a href='https://www.nerdfitness.com/blog/' target="_blank"><h1 className='underline text-center text-white font-mono mt-4'>Nerd Fitness</h1></a>
-      <a href='https://lovesweatfitness.com/blog/' target="_blank"><h1 className='underline text-center text-white font-mono mt-4'>Love Sweat Fitness</h1></a>
-      <a href='https://breakingmuscle.com/ target="_blank"'><h1 className='underline text-center text-white font-mono mt-4'>Breaking Muscles</h1></a>
-      <a href='https://www.reebok.com/us/blog?SSAID=314743&cm_mmc=Rbkaffiliates_SAS-_-314743-_-None-_-banner-_-dv%3AeCom-_-cn%3A450685-_-pc%3ANone&cm_mmc1=US&cm_mmc2=reebok-NA-eCom-Affiliates-314743-None-None-US-450685-None&dclid=CjkKEQjw8cCGBhCvz_fn4fTZ49wBEiQA_OrQTHKFeNas5Oyl2xTWg18EaqhQbGXhYl1SXfubE0dBlx3w_wcB&sscid=61k5_sqpes' target="_blank"><h1 className='underline text-center mb-16 text-white font-mono mt-4'>Reebok Fitness Blog</h1></a> */}
+      <p className='font-perm text-center text-2xl text-green-200'>Fitness Blogs</p>
+      <Link href='https://www.nerdfitness.com/blog/' target="_blank"><a href='https://www.nerdfitness.com/blog/' target="_blank"><h1 className='underline text-center text-white font-mono mt-4'>Nerd Fitness</h1></a></Link>
+      <Link href='https://lovesweatfitness.com/blog/' target="_blank"><a href='https://lovesweatfitness.com/blog/' target="_blank"><h1 className='underline text-center text-white font-mono mt-4'>Love Sweat Fitness</h1></a></Link>
+      <Link href='https://breakingmuscle.com/' target="_blank"><a href='https://breakingmuscle.com/' target="_blank"><h1 className='underline text-center text-white font-mono mt-4'>Breaking Muscles</h1></a></Link>
+      <Link href='https://www.reebok.com/us/blog?SSAID=314743&cm_mmc=Rbkaffiliates_SAS-_-314743-_-None-_-banner-_-dv%3AeCom-_-cn%3A450685-_-pc%3ANone&cm_mmc1=US&cm_mmc2=reebok-NA-eCom-Affiliates-314743-None-None-US-450685-None&dclid=CjkKEQjw8cCGBhCvz_fn4fTZ49wBEiQA_OrQTHKFeNas5Oyl2xTWg18EaqhQbGXhYl1SXfubE0dBlx3w_wcB&sscid=61k5_sqpes' target="_blank"><a href='https://www.reebok.com/us/blog?SSAID=314743&cm_mmc=Rbkaffiliates_SAS-_-314743-_-None-_-banner-_-dv%3AeCom-_-cn%3A450685-_-pc%3ANone&cm_mmc1=US&cm_mmc2=reebok-NA-eCom-Affiliates-314743-None-None-US-450685-None&dclid=CjkKEQjw8cCGBhCvz_fn4fTZ49wBEiQA_OrQTHKFeNas5Oyl2xTWg18EaqhQbGXhYl1SXfubE0dBlx3w_wcB&sscid=61k5_sqpes' target="_blank"><h1 className='underline text-center mb-16 text-white font-mono mt-4'>Reebok Fitness Blog</h1></a></Link>
 
     </>
   )
