d5495b13df1f375f8d03cff1babe82aec6f250cf
cphork <christina.phork@gmail.com>
2021-06-21 16:20:35 -0500

    added blank target to a tags

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
         <div className="lg:flex justify-center max-w-md mx-auto bg-green-500 w-96 mt-16 lg:mt-4  rounded-xl overflow-hidden md:max-w-2xl">
           <div className="md:flex">
             <div className="md:flex-shrink-0">
-              <iframe width="390" height="300" src="https://www.youtube.com/embed/OIeG3_kY8Yw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <iframe width="390" height="300" src="https://www.youtube.com/embed/OIeG3_kY8Yw" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
           </div>
         </div>
@@ -99,10 +99,10 @@ export default function Home() {
 
       {/* BLOGS LINKED TO EXTERNAL SOURCE */}
       <p className='font-perm text-center text-2xl text-green-200'>Fitness Blogs</p>
-      <a href='https://www.nerdfitness.com/blog/'><h1 className='underline text-center text-white font-mono mt-4'>Nerd Fitness</h1></a>
-      <a href='https://lovesweatfitness.com/blog/'><h1 className='underline text-center text-white font-mono mt-4'>Love Sweat Fitness</h1></a>
-      <a href='https://breakingmuscle.com/'><h1 className='underline text-center text-white font-mono mt-4'>Breaking Muscles</h1></a>
-      <a href='https://www.reebok.com/us/blog?SSAID=314743&cm_mmc=Rbkaffiliates_SAS-_-314743-_-None-_-banner-_-dv%3AeCom-_-cn%3A450685-_-pc%3ANone&cm_mmc1=US&cm_mmc2=reebok-NA-eCom-Affiliates-314743-None-None-US-450685-None&dclid=CjkKEQjw8cCGBhCvz_fn4fTZ49wBEiQA_OrQTHKFeNas5Oyl2xTWg18EaqhQbGXhYl1SXfubE0dBlx3w_wcB&sscid=61k5_sqpes'><h1 className='underline text-center mb-16 text-white font-mono mt-4'>Reebok Fitness Blog</h1></a>
+      <a href='https://www.nerdfitness.com/blog/' target="_blank"><h1 className='underline text-center text-white font-mono mt-4'>Nerd Fitness</h1></a>
+      <a href='https://lovesweatfitness.com/blog/' target="_blank"><h1 className='underline text-center text-white font-mono mt-4'>Love Sweat Fitness</h1></a>
+      <a href='https://breakingmuscle.com/ target="_blank"'><h1 className='underline text-center text-white font-mono mt-4'>Breaking Muscles</h1></a>
+      <a href='https://www.reebok.com/us/blog?SSAID=314743&cm_mmc=Rbkaffiliates_SAS-_-314743-_-None-_-banner-_-dv%3AeCom-_-cn%3A450685-_-pc%3ANone&cm_mmc1=US&cm_mmc2=reebok-NA-eCom-Affiliates-314743-None-None-US-450685-None&dclid=CjkKEQjw8cCGBhCvz_fn4fTZ49wBEiQA_OrQTHKFeNas5Oyl2xTWg18EaqhQbGXhYl1SXfubE0dBlx3w_wcB&sscid=61k5_sqpes' target="_blank"><h1 className='underline text-center mb-16 text-white font-mono mt-4'>Reebok Fitness Blog</h1></a>
 
     </>
   )
